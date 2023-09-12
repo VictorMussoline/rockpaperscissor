fd7cb00a9a680deb7cd50625b4a26abf46a1dfe2
Victor Mussoline <victor8augusto010604@hotmail.com>
2023-09-12 19:21:22 -0300

    Files organize

---
+++ script.js
@@ -0,0 +1,41 @@
+var elementos = document.querySelectorAll('.player-options div > img');
+var playerOpt = "";
+var enemyOpt = "";
+
+function resetEnemy(){
+    for(var i = 0; i < enemyOptions.length; i++){
+
+        enemyOptions[i].childNodes[0].style.opacity = 0;
+
+    }
+}
+
+function enemyPlayer(){
+    let rand = Math.floor(Math.random()*3);
+
+    const enemyOptions = document.querySelectorAll('.enemy-options div')
+    resetEnemy();
+    for(var i = 0; i < enemyOptions.length; i++){
+        if(i == rand){
+            enemyOptions[i].childNodes[0].style.opacity = 1;
+        }
+    }
+}
+
+function resetOpacityPlayer(){
+    for(var i = 0; i < elementos.length; i++){
+        elementos[i].style.opacity = 0.3;
+    };
+}
+
+for(var i = 0; i < elementos.length; i++){
+    elementos[i].addEventListener('click',function(t){
+        resetOpacityPlayer();
+        t.target.style.opacity = 1;
+        playerOpt = t.target.getAttribute('opt')
+
+        enemyPlayer();
+
+        //alert(playerOpt)
+    });
+}
